e1488b5a6c65d1f107d9ba8a45c27f4d39d2a69a
EREN BALCI <erenbalci3939@gmail.com>
2024-07-06 01:23:42 +0200

    feat: Add environment variables for AppEnv, Swagger Host, and Swagger BasePath

--- pkg/config/env.go
+++ pkg/config/env.go
@@ -7,6 +7,7 @@ import (
 )
 
 type Env struct {
+	AppEnv   string `json:"app_env" yaml:"app_env"`
 	Postgres struct {
 		Host     string `json:"host" yaml:"host"`
 		Port     string `json:"port" yaml:"port"`
@@ -15,6 +16,8 @@ type Env struct {
 		Password string `json:"password" yaml:"password"`
 	} `json:"postgres" yaml:"postgres"`
 	Swagger struct {
+		Host     string `json:"host" yaml:"host"`
+		BasePath string `json:"base_path" yaml:"base_path"`
 		User     string `json:"user" yaml:"user"`
 		Password string `json:"password" yaml:"password"`
 	} `json:"swagger" yaml:"swagger"`
@@ -24,12 +27,15 @@ func NewEnv() (*Env, error) {
 	godotenv.Load()
 	env := Env{}
 
+	env.AppEnv = os.Getenv("APP_ENV")
 	env.Postgres.Host = os.Getenv("POSTGRES_HOST")
 	env.Postgres.Port = os.Getenv("POSTGRES_PORT")
 	env.Postgres.User = os.Getenv("POSTGRES_USER")
 	env.Postgres.Database = os.Getenv("POSTGRES_DB")
 	env.Postgres.Password = os.Getenv("POSTGRES_PASSWORD")
 
+	env.Swagger.Host = os.Getenv("SWAGGER_HOST")
+	env.Swagger.BasePath = os.Getenv("SWAGGER_BASE_PATH")
 	env.Swagger.User = os.Getenv("SWAGGER_USER")
 	env.Swagger.Password = os.Getenv("SWAGGER_PASSWORD")
 
