24477e18eb0acbc27e8053ec221a8f9e52742d68
EREN BALCI <erenbalci3939@gmail.com>
2024-07-06 01:25:20 +0200

    feat: Add CORS middleware for external Swagger documentation

--- pkg/infrastructure/swagger_external.go
+++ pkg/infrastructure/swagger_external.go
@@ -1,10 +1,12 @@
 package infrastructure
 
 import (
+	docs "github.com/balcieren/go-microservice-boilerplate/docs"
 	"github.com/balcieren/go-microservice-boilerplate/pkg/config"
 	"github.com/balcieren/go-microservice-boilerplate/pkg/helper"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/basicauth"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/swagger"
 	"go.uber.org/fx"
@@ -14,12 +16,20 @@ func SwaggerExternalModule(name string) fx.Option {
 	return fx.Module(
 		"swagger",
 		fx.Provide(func(env *config.Env) *fiber.App {
+			docs.SwaggerInfo.Host = env.Swagger.Host
+			docs.SwaggerInfo.BasePath = env.Swagger.BasePath
+
 			app := fiber.New(fiber.Config{
 				AppName:      name,
 				ErrorHandler: helper.ErrorHandler,
 			})
 
 			app.Use(logger.New())
+			app.Use(cors.New(
+				cors.Config{
+					AllowOrigins: "*",
+				},
+			))
 
 			app.Get("/*", basicauth.New(basicauth.Config{
 				Authorizer: func(user, pass string) bool {
